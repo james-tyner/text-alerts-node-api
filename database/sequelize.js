7abf261ff838273ae501e866b0a52be44e7cb741
James Tyner <services@jamestyner.com>
2019-05-01 19:42:34 -0700

    updated Travis YAML and added condition to sequelize for travis environment

--- database/sequelize.js
+++ database/sequelize.js
@@ -1,8 +1,16 @@
 const dotenv = require("dotenv").config();
 const Sequelize = require("sequelize");
 
-module.exports = new Sequelize("text_alerts", process.env.DB_USER, process.env.DB_PASSWORD, {
-  host:"localhost",
-  port:3306,
-  dialect:"mysql"
-});
+
+if (process.env.ENVIRONMENT == "travis"){
+  module.exports = new Sequelize("text_alerts", "root", "", {
+    host:"127.0.0.1",
+    dialect:"mysql"
+  });
+} else {
+  module.exports = new Sequelize("text_alerts", process.env.DB_USER, process.env.DB_PASSWORD, {
+    host:"localhost",
+    port:3306,
+    dialect:"mysql"
+  });
+}
