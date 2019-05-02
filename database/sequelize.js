5b5ab7e5ed051ef8d684f9db5194e95f90901909
James Tyner <services@jamestyner.com>
2019-05-01 20:15:19 -0700

    new approach: remote connection. Probably won't work

--- database/sequelize.js
+++ database/sequelize.js
@@ -3,8 +3,9 @@ const Sequelize = require("sequelize");
 
 
 if (process.env.ENVIRONMENT == "travis"){
-  module.exports = new Sequelize("test", "testing", "testing", {
-    host:"127.0.0.1",
+  module.exports = new Sequelize("text_alerts", process.env.DB_USER, process.env.DB_PASSWORD, {
+    host:"https://annenberg-text.jamestyner.com",
+    port:3306,
     dialect:"mysql"
   });
 } else {
